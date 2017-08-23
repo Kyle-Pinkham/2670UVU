f46969e670003f784a2d5c54213ccdcb3603228c
Unknown <kyledpinkham@Gmail.com>
2017-08-23 13:17:11 -0600

    ...

--- New Unity Project 1/Assets/Scripts/TestScript.cs
+++ New Unity Project 1/Assets/Scripts/TestScript.cs
@@ -13,6 +13,7 @@ public class TestScript : MonoBehaviour {
         print(Time.deltaTime);
         print(clockface);
         // printing time.deltatime counts as a clock face anthony. Dont judge me.
+        //having to do all these commits... it fills me with determination.
 
 	}
 	
