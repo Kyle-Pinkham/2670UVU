670ce039d0fd577cafd2f475cf4f62ab8887c738
Unknown <kyledpinkham@Gmail.com>
2017-08-23 07:22:19 -0600

    Changed "True need" to "Purpose of" for gramatical continuity

--- New Unity Project 1/Assets/Scripts/TestScript.cs
+++ New Unity Project 1/Assets/Scripts/TestScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour {
 
-    //Adding a comment that questions the true need for having 50+ commits on a project with no clear goal yet.
+    //Adding a comment that questions the purpose of having 50+ commits on a project with no clear goal yet.
 
 	// Use this for initialization
 	void Start () {
