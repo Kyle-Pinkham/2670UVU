6d1c76a3e0ea0a29f57eb5b4549273792d2c8dee
Unknown <kyledpinkham@Gmail.com>
2017-08-23 07:21:32 -0600

    //Adding a comment that questions the true need for having 50+ commits on a project with no clear goal yet.

--- New Unity Project 1/Assets/Scripts/TestScript.cs
+++ New Unity Project 1/Assets/Scripts/TestScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour {
 
+    //Adding a comment that questions the true need for having 50+ commits on a project with no clear goal yet.
+
 	// Use this for initialization
 	void Start () {
 		
