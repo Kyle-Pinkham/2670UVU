a2bdd0945fe6764d66940f6cae05c57d6fa25da0
Unknown <kyledpinkham@Gmail.com>
2017-08-23 13:18:23 -0600

    wtf are all these "a"s

--- New Unity Project 1/Assets/Scripts/TestScript.cs
+++ New Unity Project 1/Assets/Scripts/TestScript.cs
@@ -7,13 +7,21 @@ public class TestScript : MonoBehaviour {
     //Adding a comment that questions the purpose of having 50+ commits on a project with no clear goal yet.
 
         int clockface;
-	// Use this for initialization
-	void Start () {
+    int A = 1;
+    int A = 1;
+    int A = 1;
+    int A = 1;
+    int A = 1;
+
+    // Use this for initialization
+    void Start () {
 
         print(Time.deltaTime);
         print(clockface);
         // printing time.deltatime counts as a clock face anthony. Dont judge me.
         //having to do all these commits... it fills me with determination.
+
+
 
 	}
 	
