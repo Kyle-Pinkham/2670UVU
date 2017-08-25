f291a07ecbc3213010ea15b4f1f851dfdf4b98c7
Unknown <kyledpinkham@Gmail.com>
2017-08-25 14:01:49 -0600

    committing class assets

---
+++ Scripting2670/Assets/Prefabs/Skripts/MoveInputs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class MoveInputs : MonoBehaviour {
+
+    Action KeyAction;
+	// Use this for initialization
+	void Start () {
+        KeyAction = Move;
+	}
+
+    private void Move()
+    {
+            print("left arrow pushed BOIIIII");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            KeyAction();
+        }
+	}
+}
