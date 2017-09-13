bca5e2023b420b997089ffe9f3db0a4041b4be62
Unknown <kyledpinkham@Gmail.com>
2017-09-13 12:04:17 -0600

    commit

---
+++ Scripting2670/Assets/Skripts/LoadScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+public class LoadScript : MonoBehaviour {
+
+    public static Action EndButtons;
+
+    void Start()
+    {
+        LoadScript.EndButtons += EndThis;
+    }
+
+    private void EndThis()
+    {
+        GetComponent<Button>().interactable = false;
+    }
+
+    public string scene;
+
+	public void LoadLevel()
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+        EndButtons();
+    }
+}
